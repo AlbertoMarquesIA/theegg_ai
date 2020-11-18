da377f6b96dab4931ca58cb353e74f82b3e34d3e
Alberto Marques Martinez <Alberto@MacBook-Pro-de-Alberto-3.local>
2020-11-18 19:47:44 +0100

    Correccion primera funcion incluyendo variable globales

--- Tarea_22/Tarea_22.py
+++ Tarea_22/Tarea_22.py
@@ -1,12 +1,19 @@
+a_Pesos = []
+a_Produccion = []
+N_Vacas = 0
+Peso_Max = 0.0
 def IntroduccionDatos():
-    NVacas = int(input("Introduzca la cantidad de vacas disponibles:"))
-    PesoMax = float(input("Introduzca el peso máximo del camión:"))
-    aPesos = []
-    aProduccion = []
-    for CadaVaca in range(NVacas):
-        Peso = float(input("Peso de la vaca {}:".format(CadaVaca+1)))
-        aPesos.append(Peso)
-        Produccion = float(input("Producción de la vaca {}:".format(CadaVaca+1)))
-        aProduccion.append(Produccion)
-
+    global N_Vacas
+    global Peso_Max
+    global a_Pesos
+    global a_Produccion
+    N_Vacas = int(input("Introduzca la cantidad de vacas disponibles:  "))
+    Peso_Max = float(input("Introduzca el peso máximo del camión:  "))
+    for CadaVaca in range(N_Vacas):
+        Peso = float(input("Peso de la vaca {}:  ".format(CadaVaca+1)))
+        a_Pesos.append(Peso)
+        Produccion = float(input("Producción de la vaca {}:  ".format(CadaVaca+1)))
+        a_Produccion.append(Produccion)
 IntroduccionDatos()
+print(a_Pesos)
+print(a_Produccion)
